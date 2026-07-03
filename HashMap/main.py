9a8ddf3d67b7cc9b70d5a76fb9f6233e725c03c0
petrash-misha <petrash.mihail@gmail.com>
2026-07-03 17:19:15 +0100

    New functions added and function Add and Delete updated

--- HashMap/main.py
+++ HashMap/main.py
@@ -1,3 +1,6 @@
+import copy
+from collections.abc import Callable
+
 class Node:
     key = None
     value = None
@@ -7,54 +10,103 @@ class Node:
         self.value = value
 
 class HashMap:
-    array = [None] * 26
+    _initial_size = 10
+    _coefficient = 3
+    _decrowding_coefficient = 0.35
+    _crowding_coefficient = 0.65
+    _count = 0
+    _array = []
 
-    def h(self,data, limit):
+    def __init__(self):
+        self.clear()
+
+    def _h(self, data, limit):
         return hash(data) % limit
 
     def add(self, key, value):
-        node = Node(key, value)
-        i = self.h(key, len(self.array))
+        if (self._count / len(self._array)) >= self._crowding_coefficient:
+            self._re_hash(len(self._array) * self._coefficient)
 
-        while self.array[i] != None:
-            i += 1
-            if i == len(self.array):
+        node = Node(key, value)
+        self._add_node(node)
+        self._count += 1
+
+    def _add_node(self, node):
+        i = self._h(node.key, len(self._array))
+
+        while self._array[i] != None:
+            i = self._h(node.key, len(self._array))
+            if i == len(self._array):
                 i = 0
 
+        self._array[i] = node
 
-        self.array[i] = node
+
+    def _re_hash(self, new_size):
+        old_array = self._array
+        self._array = [None] * new_size
+        for node in old_array:
+            if node != None:
+                self._add_node(node)
 
     def get(self, key):
-        i = self.h(key, len(self.array))
+        i = self._find(key)
+        if i != None:
+            return self._array[i].value
+        return None
+
+    def _find(self, key):
+        i = self._h(key, len(self._array))
         ind = i
-        node = self.array[i]
+        node = self._array[i]
         while node == None or node.key != key:
             i += 1
             if ind == i:
                 return None
-            if i == len(self.array):
+            if i == len(self._array):
                 i = 0
-            node = self.array[i]
+            node = self._array[i]
+        return i
+
+    def clear(self):
+        self._array = [None] * self._initial_size
+        self._count = 0
+
+    def delete(self, key):
+        i = self._find(key)
+        if i == None:
+            return None
+        node = self._array[i]
+        self._array[i] = None
+        self._count -= 1
+
+        new_size = len(self._array) // self._coefficient
+        if new_size >= self._initial_size and self._count / new_size <= self._decrowding_coefficient:
+            self._re_hash(new_size)
 
         return node.value
 
+    def update(self, key, new_value):
+        i = self._find(key)
+        if i == None:
+            return False
+        self._array[i].value = new_value
+        return True
 
+    def each(self, func: Callable[[any, any], None]):
+        for node in self._array:
+            if node != None:
+                func(node.key, node.value)
 
+    def count(self):
+        return self._count
 
-    # def delete(self, key):
-    #
-    # def update(self, key, new_value):
-    #
-    # def each(self, func):
-    #
-    # def clear(self):
-    #
-    # def count(self):
-    #
-    # def clone(self):
-    #
-    # def deep_clone(self):
+    def copy(self):
+        hashMap = HashMap()
+        self.each(hashMap.add)
+        return hashMap
 
-
-
-
+    def deepcopy(self):
+        hashMap = HashMap()
+        self.each(lambda k, v: hashMap.add(copy.deepcopy(k), copy.deepcopy(v)))
+        return hashMap
--- HashMap/test.py
+++ HashMap/test.py
@@ -6,6 +6,9 @@ hash_map = main.HashMap()
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z']
 
+print(hash_map._count)
+
+
 num = 0
 for i in letters:
     num += 1
@@ -17,7 +20,58 @@ for i in letters:
     v = hash_map.get(i)
     print(f"is {i} = {num} :", v == num)
 
-print(hash_map.array)
+print(hash_map._array)
+print(len(hash_map._array))
+print(hash_map._count)
+
+print("----------------------------------------------")
+num = -26
+for i in letters:
+    v = hash_map.update(i, num)
+    print(f"is {i} = {num} :", v)
+    num += 1
+
+print(len(hash_map._array))
+print(hash_map._array)
+print(hash_map._count)
+
+print("----------------------------------------------")
+
+
+num = -26
+for i in letters:
+    v = hash_map.delete(i)
+    print(f"is {i} deleted :", v == num)
+    num += 1
+
+
+print(len(hash_map._array))
+print(hash_map._array)
+print(hash_map._count)
+
+print("----------------------------------------------")
+num = 0
+for i in letters:
+    num += 1
+    hash_map.add(i, num)
+
+num = 0
+for i in letters:
+    num += 1
+    v = hash_map.get(i)
+    print(f"is {i} = {num} :", v == num)
+
+print(len(hash_map._array))
+print(hash_map._array)
+print(hash_map._count)
+
+print("----------------------------------------------")
+
+hash_map.clear()
+print(len(hash_map._array))
+print(hash_map._array)
+print(hash_map._count)
+
 
 
 
