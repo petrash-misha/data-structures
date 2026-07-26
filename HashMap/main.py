8275e660ab51ea14a39d863a6387ae05f55af573
petrash-misha <petrash.mihail@gmail.com>
2026-07-26 21:09:00 +0100

    Hash function changed due to not working as it should have

--- HashMap/main.py
+++ HashMap/main.py
@@ -1,5 +1,7 @@
 import copy
 from collections.abc import Callable
+import math
+
 
 class Node:
     key = None
@@ -9,19 +11,39 @@ class Node:
         self.key = key
         self.value = value
 
+def _to_int(data):
+    return int.from_bytes(data.encode('utf-8'), byteorder='big')
+
 class HashMap:
-    _initial_size = 10
+    _initial_size = 13
     _coefficient = 3
-    _decrowding_coefficient = 0.35
-    _crowding_coefficient = 0.65
+    _decrowding_coefficient = 0.25
+    _crowding_coefficient = 0.75
     _count = 0
     _array = []
 
+
     def __init__(self):
         self.clear()
+        self._prime = self.__get_largest_prime(len(self._array))
 
-    def _h(self, data, limit):
-        return hash(data) % limit
+    def __get_largest_prime(self, limit: int) -> int:
+        is_prime = [True] * (limit + 1)
+        is_prime[0], is_prime[1] = False, False
+        for i in range(2, int(math.sqrt(limit)) + 1):
+            if is_prime[i]:
+                for j in range(i * i, limit + 1, i):
+                    is_prime[j] = False
+        for i in range(limit, -1, -1):
+            if is_prime[i]:
+                return i
+
+
+    def _h1(self, data):
+        return _to_int(data) % len(self._array)
+
+    def _h2(self, key):
+        return self._prime - (_to_int(key) % self._prime)
 
     def add(self, key, value):
         if (self._count / len(self._array)) >= self._crowding_coefficient:
@@ -32,19 +54,23 @@ class HashMap:
         self._count += 1
 
     def _add_node(self, node):
-        i = self._h(node.key, len(self._array))
+        hash1 = self._h1(node.key)
+        hash2 = self._h2(node.key)
+        index = hash1
+        i = 1
 
-        while self._array[i] != None:
-            i = self._h(node.key, len(self._array))
-            if i == len(self._array):
-                i = 0
+        while self._array[index] != None:
+            index = (hash1 + i * hash2) % len(self._array)
+            i += 1
 
-        self._array[i] = node
+        self._array[index] = node
 
 
     def _re_hash(self, new_size):
         old_array = self._array
         self._array = [None] * new_size
+        self._prime = self.__get_largest_prime(len(self._array))
+        # print("PRIME: ", self._prime)
         for node in old_array:
             if node != None:
                 self._add_node(node)
@@ -55,18 +81,19 @@ class HashMap:
             return self._array[i].value
         return None
 
+
     def _find(self, key):
-        i = self._h(key, len(self._array))
-        ind = i
-        node = self._array[i]
-        while node == None or node.key != key:
+        hash1 = self._h1(key)
+        hash2 = self._h2(key)
+        index = hash1
+        i = 1
+
+        while self._array[index] != None:
+            if self._array[index].key == key:
+                return index
+            index = (hash1 + i * hash2) % len(self._array)
             i += 1
-            if ind == i:
-                return None
-            if i == len(self._array):
-                i = 0
-            node = self._array[i]
-        return i
+        return None
 
     def clear(self):
         self._array = [None] * self._initial_size
